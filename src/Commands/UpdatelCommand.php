5ec6ef9ab9875b4428074c35090bc304567f8e71
never615 <583531034@qq.com>
2017-10-19 18:35:24 +0800

    库的升级命令编写

---
+++ src/Commands/UpdatelCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Overtrue\LaravelWechat\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdatelCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'wechat:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update the wechat package';
+
+    /**
+     * Install directory.
+     *
+     * @var string
+     */
+    protected $directory = '';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $this->call('migrate', ['--path' => str_replace(base_path(), '', __DIR__).'/../../migrations/']);
+    }
+
+}
