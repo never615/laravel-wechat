7b8996fe1a56037538ab129d32435aa9e2860695
never615 <583531034@qq.com>
2018-01-03 19:07:46 +0800

    no message

--- src/Controllers/GongzhonghaoController.php
+++ src/Controllers/GongzhonghaoController.php
@@ -3,8 +3,10 @@
 namespace Overtrue\LaravelWechat\Controllers;
 
 use EasyWeChat\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Mallto\Tool\Exception\ResourceException;
+use Mallto\Tool\Utils\ResponseUtils;
 use Overtrue\LaravelWechat\WechatUtils;
 
 /**
@@ -13,7 +15,7 @@ use Overtrue\LaravelWechat\WechatUtils;
  * Date: 19/04/2017
  * Time: 7:01 PM
  */
-class JsconfigController extends \Illuminate\Routing\Controller
+class GongzhonghaoController extends \Illuminate\Routing\Controller
 {
     /**
      * @var WechatUtils
@@ -30,6 +32,47 @@ class JsconfigController extends \Illuminate\Routing\Controller
         $this->wechatUtils = $wechatUtils;
     }
 
+
+    /**
+     * 获得用户微信的授权信息,授权中转站
+     *
+     * 返回openid
+     */
+    public function oauth(Request $request)
+    {
+
+        $redirectUrl = $request->redirect_url;
+        $wechatUser = session('wechat.oauth_user');
+
+        $cryptOpenId = encrypt($wechatUser->id);
+
+        return ResponseUtils::responseBasicByRedirect($redirectUrl, ["openid" => $cryptOpenId]);
+    }
+
+//    /**
+//     * 重定向携带微信用户信息
+//     *
+//     * @param Request $request
+//     * @return \App\Lib\Redirect
+//     */
+//    public function oauthInfo(Request $request)
+//    {
+//        $redirectUrl = $request->redirect_url;
+//        $wechatUser = session('wechat.oauth_user');
+//
+//        return ResponseUtils::responseBasicByRedirect2($redirectUrl, $wechatUser);
+//    }
+
+    /**
+     * 测试
+     *
+     * @param Request $request
+     */
+    public function userTest(Request $request)
+    {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+        dd($user);
+    }
 
     /**
      * 公众号获取jssdk配置
